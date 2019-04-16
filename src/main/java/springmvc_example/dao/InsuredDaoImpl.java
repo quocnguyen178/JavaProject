8f92105d5702a4968c8179ec3874c5499d113b5a
Quoc Nguyen <minhquoc.github@gmail.com>
2019-04-16 11:47:27 +0700

    Fix function infoInsured

--- src/main/java/springmvc_example/dao/InsuredDaoImpl.java
+++ src/main/java/springmvc_example/dao/InsuredDaoImpl.java
@@ -56,13 +56,16 @@ public class InsuredDaoImpl implements InsuredDao {
 				js = new JSONObject(cursor.next().toString());
 				JSONObject id = js.getJSONObject("_id");
 				String stringId = (String) id.get("$oid");
-				JSONArray storeList = js.getJSONArray("quotation_insured");
-				String _id = "";
-				for (int i = 0; i < storeList.length(); i++) {
-					JSONObject info = storeList.getJSONObject(i);
-					_id = info.getString("_id");
-					if (insuredId.equals(_id)&& mongoId.equals(stringId))
-						objectInfo = info;
+				//if vao dung quote thi moi duyet toi mang insured
+				if(stringId.equals(mongoId)) {
+					JSONArray storeList = js.getJSONArray("quotation_insured");
+					String _id = "";
+					for (int i = 0; i < storeList.length(); i++) {
+						JSONObject info = storeList.getJSONObject(i);
+						_id = info.getString("_id");
+						if (insuredId.equals(_id))
+							objectInfo = info;
+					}
 				}
 			} catch (JSONException e) {
 				e.printStackTrace();
@@ -77,20 +80,16 @@ public class InsuredDaoImpl implements InsuredDao {
 		BasicDBObject set = new BasicDBObject();
 		BasicDBObject update = new BasicDBObject();
 		
-		boolean isInsured = json_request.getJSONObject("quote_owner_same_as_insured").getBoolean("value");
-		String gender = json_request.getJSONObject("quote_owner_gender").getString("value");
-		String residenceState = json_request.getJSONObject("quote_owner_residence_state").getString("value");
-		String dateOfBirth = json_request.getString("quote_owner_date_of_birth");
-		String occupation = json_request.getJSONObject("quote_owner_occupation").getString("value");
-		String annualIncome = json_request.getJSONObject("quote_owner_annual_income").getString("value");
-		String annualIncomeMin = json_request.getJSONObject("quote_owner_annual_income").getString("minvalue");
-		String annualIncomeMax = json_request.getJSONObject("quote_owner_annual_income").getString("maxvalue");
-		String smokingStatus = json_request.getJSONObject("quote_owner_smoking_status").getString("value");
-		String givenName = json_request.getString("quote_owner_given_name");
-		String lastName = json_request.getString("quote_owner_last_name");
-		String title = json_request.getJSONObject("quote_owner_title").getString("value");
-		String nationality = json_request.getJSONObject("quote_owner_nationality").getString("value");
-		String marialStatus = json_request.getJSONObject("quote_owner_marital_status").getString("value");
+		boolean isInsured = json_request.getBoolean("quote-owner-same-as-insured");
+		String gender = json_request.getString("quote-owner-gender");
+		String residenceState = json_request.getString("quote-owner-residence-state");
+		String dateOfBirth = json_request.getString("quote-owner-birth-date");
+		String occupation = json_request.getString("quote-owner-occupation");
+		String annualIncome = json_request.getString("quote-owner-annual-income");
+		String smokingStatus = json_request.getString("quote-owner-smoking-status");
+		String givenName = json_request.getString("quote-owner-given-name");
+		String lastName = json_request.getString("quote-owner-last-name");
+		String title = json_request.getString("quote-owner-title");
 		if (isInsured) {
 			
 			query.put("quotation_insured._id", insuredId);
@@ -107,8 +106,7 @@ public class InsuredDaoImpl implements InsuredDao {
 			set.put("quotation_insured.$.insured_residence_state.value", residenceState);
 			set.put("quotation_insured.$.insured_given_name", givenName);
 			set.put("quotation_insured.$.insured_annual_income.value", annualIncome);
-			set.put("quotation_insured.$.insured_annual_income.minvalue", annualIncomeMin);
-			set.put("quotation_insured.$.insured_annual_income.maxvalue", annualIncomeMax);
+			set.put("quotation_insured.$.insured_title.value", title);
 			update.put("$set", set);
 			mongoTemplate.getCollection(COLLECTION_NAME).update(query, update);
 		}
