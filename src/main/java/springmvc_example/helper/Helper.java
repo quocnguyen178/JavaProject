93c2e35f80975b986cbd0a5e24dc68b7ea8c931e
Quoc Nguyen <minhquoc.github@gmail.com>
2019-04-16 11:50:01 +0700

    Fix file helper

--- src/main/java/springmvc_example/helper/Helper.java
+++ src/main/java/springmvc_example/helper/Helper.java
@@ -9,12 +9,12 @@ public class Helper {
 	public JSONObject generateId(JSONObject document) {
 		try {
 			ObjectId _id = new ObjectId();
-			String sub_id = "id" + _id.getTimestamp();
+//			String sub_id = "id" + _id.getTimestamp();
 			String primary_in = "in" + _id.getTimestamp();
 			String primary_ow = "ow" + _id.getTimestamp();
 			String primary_co = "co" + _id.getTimestamp();
 			
-			document.put("sub_id", sub_id);
+//			document.put("sub_id", sub_id);
 			if (document.getJSONArray("quotation_insured") != null) {
 				JSONObject array_insured_putid = document.getJSONArray("quotation_insured").getJSONObject(0).put("_id", primary_in);
 			}
